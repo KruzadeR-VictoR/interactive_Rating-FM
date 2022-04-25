ae32d9cda09f0e3ad7ce8385603048dbe4877f3f
KruzadeR-VictoR <victorbiju31@gmail.com>
2022-04-25 15:42:35 +0530

    initial Commit

--- app.js
+++ app.js
@@ -2,23 +2,22 @@ const submitBtn = document.querySelector(".Submit");
 const cardContainer = document.querySelector(".card-container");
 const rating = document.querySelector(".rating");
 const li = document.querySelector(".pagination");
-let value = 0;
-console.log(li);
-
+let value = "";
 const ratingVal = (e) => {
-  localStorage.setItem("value", e.target.value);
+  value = e.target.innerText;
 };
-let thankYouCard = `<div class="card-thankYou">
+
+const onSubmit = () => {
+  let thankYouCard = `<div class="card-thankYou">
 <img class="card-2" src="./images/illustration-thank-you.svg" alt="" />
-<p class="rating">You selected ${localStorage.getItem("value")} out of 5</p>
+<p class="rating">You selected ${value} out of 5</p>
 <h1>Thank you!</h1>
 <p class="tyMessage">
   We appreciate you taking the time to give a rating. If you ever need more support, 
 don’t hesitate to get in touch!
 </p>        
 </div>`;
-const onSubmit = () => {
   cardContainer.innerHTML = thankYouCard;
-    rating.innerText = `You selected ${localStorage.getItem(value)} out of 5`;
-  localStorage.clear();
 };
+
+submitBtn.addEventListener("click", onSubmit);
